a452ab2a612e545cdb32b41d5a179a39ebf3bd3f
ejprok <ejprok@gmail.com>
2019-03-12 04:08:49 +0000

    merged the models.py for the home app

--- csforsac/home/models.py
+++ csforsac/home/models.py
@@ -8,7 +8,6 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 
-<<<<<<< HEAD
 class HomePage(Page):
     carousel_1_header = models.TextField(max_length=255, blank=True)
     carousel_1_body = models.TextField(max_length=255, blank=True)
@@ -27,41 +26,15 @@ class HomePage(Page):
         FieldPanel('carousel_3_header'),
         FieldPanel('carousel_3_body'),
     ]
-=======
-class HomePage(RoutablePageMixin, Page):
-    body = models.TextField(max_length=255)
-    test_field = models.CharField(max_length=30, blank=True)
-    extra = RichTextField(blank=True)
-    more = RichTextField(blank=True)
-    content_panels = Page.content_panels + [
-        FieldPanel('body'),
-        FieldPanel('extra'),
-        FieldPanel('test_field'),
-        FieldPanel('more'),
-    ]
-    @route(r'^home/$')
-    def home_page(self, request, *args, **kwargs):
-        
-        response = TemplateResponse(
-            request, 'home/contact_page.html'
-        )
-        return response
 
 class AboutPage(RoutablePageMixin, Page):
     body = models.CharField(max_length=255, blank=True)
-    test_field = models.CharField(max_length=30, blank=True)
-    extra = RichTextField(blank=True)
-    more = RichTextField(blank=True)
     content_panels = Page.content_panels + [
         FieldPanel('body'),
-        FieldPanel('extra'),
-        FieldPanel('test_field'),
-        FieldPanel('more'),
     ]
  
     @route(r'^about/$')
     def about_page(self, request, *args, **kwargs):
-        print("ye")
         response = TemplateResponse(
             request, 'home/about_page.html'
         )
@@ -69,4 +42,17 @@ class AboutPage(RoutablePageMixin, Page):
 
 
 
->>>>>>> 2c0ed111059f52667476270a53cdf979744af2a6
+class ContactPage(RoutablePageMixin, Page):
+    body = models.CharField(max_length=255, blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+ 
+    @route(r'^contact/$')
+    def about_page(self, request, *args, **kwargs):
+        response = TemplateResponse(
+            request, 'home/contact_page.html'
+        )
+        return response
+
+
